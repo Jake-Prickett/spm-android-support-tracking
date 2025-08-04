ab945fddcfe43ae2b6969dd145f086cec3758130
Jake Prickett <jprickett821@gmail.com>
2025-08-04 00:14:29 -0400

    Experiment 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/spm-android-support-tracking' : '',
-  assetPrefix: isProd ? '/spm-android-support-tracking/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
